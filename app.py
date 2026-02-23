caed098ef88598fd18f2e87b27bf012414b80f97
Cellebrimbor <alhnu@vk.com>
2026-02-23 23:25:42 +0300

    С\eeраняю изменения перед pull

--- app.py
+++ app.py
@@ -1037,4 +1037,4 @@ with app.app_context():
 
 # Запуск приложения
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5004)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- files/content_server.py
+++ files/content_server.py
@@ -489,4 +489,4 @@ with app.app_context():
 
 # Запуск приложения
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5005)
+    app.run(debug=True, host='0.0.0.0', port=5001)
